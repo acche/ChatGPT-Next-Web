6bb5196dbb421186ed6822bc4f82104ef02175f2
Chen Chen <caxche@gmail.com>
2024-01-02 21:33:51 +0800

    Revert "Update route.ts"

--- app/api/google/[...path]/route.ts
+++ app/api/google/[...path]/route.ts
@@ -101,16 +101,21 @@ export const POST = handle;
 
 export const runtime = "edge";
 export const preferredRegion = [
-  "sfo1",
-  "cle1",
-  "iad1",
-  "pdx1",
+  "arn1",
   "bom1",
+  "cdg1",
+  "cle1",
   "cpt1",
+  "dub1",
+  "fra1",
   "gru1",
   "hnd1",
+  "iad1",
   "icn1",
   "kix1",
+  "lhr1",
+  "pdx1",
+  "sfo1",
   "sin1",
-  "syd1"
+  "syd1",
 ];
